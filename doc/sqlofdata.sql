d6a57533469bf0a4d557086d09d8b02f9fe9f137
Vianliu <90883274+Vianliu@users.noreply.github.com>
2024-04-02 20:54:12 -0500

    littel change

--- doc/sqlofdata.sql
+++ doc/sqlofdata.sql
@@ -1,30 +1,34 @@
-CREATE TABLE CarTeams (
-    TeamID INT AUTO_INCREMENT,
-    UserID INT NOT NULL,
-    AssembleDate DATE NOT NULL,
-    PRIMARY KEY (TeamID)
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    UserName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL
 );
-CREATE TABLE sales (
-    SaleID INT AUTO_INCREMENT PRIMARY KEY,
-    ListingID INT,
-    BuyerUserID INT,  -- 购买者的UserID
-    SalePrice DECIMAL(10, 2),  -- 销售价格
-    SaleDate DATE,  -- 销售日期
-    -- 从listing表复制的其他字段
-    CarID INT,
-    Company VARCHAR(255),
-    YearOfPurchase YEAR,
-    KMsDriven INT,
-    RegistrationCity VARCHAR(255),
-    Condition VARCHAR(100),
-    SellerLocation VARCHAR(255),
-    Features TEXT,
-    ImageURLs TEXT,
-    Mileage DECIMAL(10, 2),
-    Trim VARCHAR(100),
-    Year YEAR,
-    ZipCode VARCHAR(20),
-    FOREIGN KEY (ListingID) REFERENCES listing(ListingID),
-    FOREIGN KEY (BuyerUserID) REFERENCES users(UserID)  -- 假设有一个用户表来存储UserID
+CREATE TABLE CarModels (
+    CarID INT PRIMARY KEY,
+    Make VARCHAR(255) NOT NULL,
+    Model VARCHAR(255) NOT NULL,
+    Year YEAR NOT NULL,
+    KMs_driven INT NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    FuelType VARCHAR(50) NOT NULL,
+    Transmission VARCHAR(50) NOT NULL
 );
-
+CREATE TABLE Recalls (
+    RecallID INT AUTO_INCREMENT PRIMARY KEY,
+    ReportReceivedDate DATE NOT NULL,
+    Brand VARCHAR(255) NOT NULL,
+    Reason TEXT,
+    Component VARCHAR(255),
+    ConsequenceSummary TEXT
+);
+CREATE TABLE CarHistory (
+    VIN VARCHAR(17) PRIMARY KEY,
+    Brand VARCHAR(50) NOT NULL,
+    Model VARCHAR(50) NOT NULL,
+    Year YEAR NOT NULL,
+    Title_Status VARCHAR(50) NOT NULL,
+    Mileage INT NOT NULL,
+    Color VARCHAR(50) NOT NULL,
+    State VARCHAR(50) NOT NULL
+);

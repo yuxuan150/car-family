f91f408a19b1771076a0e5ffdd3a9be0301ab538
Vianliu <90883274+Vianliu@users.noreply.github.com>
2024-04-03 22:49:45 -0500

    sqltabgle

--- doc/sqlofdata.sql
+++ doc/sqlofdata.sql
@@ -14,21 +14,43 @@ CREATE TABLE CarModels (
     FuelType VARCHAR(50) NOT NULL,
     Transmission VARCHAR(50) NOT NULL
 );
-CREATE TABLE Recalls (
-    RecallID INT AUTO_INCREMENT PRIMARY KEY,
-    ReportReceivedDate DATE NOT NULL,
-    Brand VARCHAR(255) NOT NULL,
-    Reason TEXT,
-    Component VARCHAR(255),
-    ConsequenceSummary TEXT
+CREATE TABLE location (
+  location_id SERIAL PRIMARY KEY,
+  city_name VARCHAR(50) NOT NULL,
+  latitude FLOAT NOT NULL,
+  longitude FLOAT NOT NULL
 );
-CREATE TABLE CarHistory (
-    VIN VARCHAR(17) PRIMARY KEY,
-    Brand VARCHAR(50) NOT NULL,
-    Model VARCHAR(50) NOT NULL,
-    Year YEAR NOT NULL,
-    Title_Status VARCHAR(50) NOT NULL,
-    Mileage INT NOT NULL,
-    Color VARCHAR(50) NOT NULL,
-    State VARCHAR(50) NOT NULL
+CREATE TABLE car (
+  car_id SERIAL PRIMARY KEY,
+  brand VARCHAR(100) NOT NULL,
+  model VARCHAR(255) NOT NULL,
+  body_type VARCHAR(25) NOT NULL,
+  car_type VARCHAR(25) NOT NULL,
+  year INT NOT NULL,
+  price INT NOT NULL
+);
+CREATE TABLE user_account (
+  user_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  contact VARCHAR(15) NOT NULL,
+  location_id INT NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES location(location_id)
+);
+CREATE TABLE advertisement (
+  ad_id SERIAL PRIMARY KEY,
+  car_id INT NOT NULL,
+  user_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  FOREIGN KEY (car_id) REFERENCES car(car_id),
+  FOREIGN KEY (user_id) REFERENCES user_account(user_id)
+);
+CREATE TABLE bid (
+  bid_id SERIAL PRIMARY KEY,
+  car_id INT NOT NULL,
+  user_id INT NOT NULL,
+  date_bid DATE NOT NULL,
+  bid_price INT NOT NULL,
+  bid_status VARCHAR(25) NOT NULL,
+  FOREIGN KEY (car_id) REFERENCES car(car_id),
+  FOREIGN KEY (user_id) REFERENCES user_account(user_id)
 );
